f413f6d63186518b6de4715eb971cecd4f98f0ce
Krzysztof Karasiński <krzysiek1105@opoczta.pl>
2022-02-21 18:27:36 +0100

    Add transaction pipeline behavior.

---
+++ Generic.Shared/Application/PipelineBehaviors/TransactionPipelineBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Transactions;
+using MediatR;
+
+namespace Generic.Shared.Application.PipelineBehaviors;
+
+internal class TransactionPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    {
+        using var transactionScope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+        var response = await next();
+        transactionScope.Complete();
+
+        return response;
+    }
+}
--- Generic.Shared/DependencyInjection.cs
+++ Generic.Shared/DependencyInjection.cs
@@ -1,5 +1,7 @@
-﻿using Generic.Shared.Domain;
+﻿using Generic.Shared.Application.PipelineBehaviors;
+using Generic.Shared.Domain;
 using Generic.Shared.Infrastructure;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Generic.Shared;
@@ -9,6 +11,7 @@ public static class DependencyInjection
     public static IServiceCollection AddSharedModule(this IServiceCollection services)
     {
         services.AddTransient<IEmailSender, EmailSender>();
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(TransactionPipelineBehavior<,>));
 
         return services;
     }
