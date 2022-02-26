10a9220a8f617d7b36b8182e91f6ff1ee168da83
Krzysztof Karasiński <krzysiek1105@opoczta.pl>
2022-02-26 14:07:21 +0100

    Last name value object implementation with tests.

---
+++ Generic.Shared/Application/FailureReasons/DomainRuleBroken.cs
@@ -0,0 +1,11 @@
+﻿namespace Generic.Shared.Application.FailureReasons;
+
+public class DomainRuleBroken : ICommandFailureReason
+{
+    public string Description { get; }
+
+    public DomainRuleBroken(string reason)
+    {
+        Description = reason;
+    }
+}
--- Users/Generic.Users.Domain/LastName.cs
+++ Users/Generic.Users.Domain/LastName.cs
@@ -4,15 +4,39 @@ namespace Generic.Users.Domain;
 
 public class LastName : ValueObject
 {
+    public const int MinLength = 2;
+    public const int MaxLength = 50;
     public string Value { get; }
 
-    public LastName(string value)
+    public LastName(string lastName)
     {
-        Value = value;
+        ArgumentNullException.ThrowIfNull(lastName, nameof(lastName));
+
+        if (string.IsNullOrEmpty(lastName))
+        {
+            throw new DomainException("Last name is empty");
+        }
+
+        if (lastName.Length < MinLength)
+        {
+            throw new DomainException($"Last name must be at least {MinLength} characters long");
+        }
+
+        if (lastName.Length > MaxLength)
+        {
+            throw new DomainException($"Last name must be at most {MaxLength} characters long");
+        }
+
+        if (!lastName.All(char.IsLetter))
+        {
+            throw new DomainException($"Last name must be only letters");
+        }
+
+        Value = lastName;
     }
 
     protected override IEnumerable<object> GetEqualityComponents()
     {
-        yield return Value;
+        yield return Value.ToUpper();
     }
 }
